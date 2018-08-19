985d7c3a158808ba6ab30a7c3faf314d6116e82d
jamnopapa <jamnopapa@gmail.com>
2018-08-19 10:06:09 +0900

    Update LED8a.sh

--- LED8a.sh
+++ LED8a.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+set_di1r() {
+for i in 0 1 2 3 4 5 6 7
+  do
+    echo 33$i > /sys/class/gpio/export
+    echo out > /sys/class/gpio/gpio33$i/direction
+  done
+}
+
+led_onoff1() {
+while [ true ];
+  do
+    for i in 0 1 2 3 4 5 6 7
+      do
+        echo  1  > /sys/class/gpio/gpio33$i/value
+        sleep 0.01
+      done
+      sleep 0.5
+    for i in 7 6 5 4 3 2 1 0
+      do
+        echo  0  > /sys/class/gpio/gpio33$i/value
+        sleep 0.01
+      done
+      sleep 0.5
+  done
+}
+
+set_dir1
+led_onoff1
 
 
 dir="none"
@@ -87,31 +115,3 @@ fi
 read_gpio $gpio
 
 
-set_dir() {
-for i in 0 1 2 3 4 5 6 7
-  do
-    echo 33$i > /sys/class/gpio/export
-    echo out > /sys/class/gpio/gpio33$i/direction
-  done
-}
-
-led_onoff() {
-while [ true ];
-  do
-    for i in 0 1 2 3 4 5 6 7
-      do
-        echo  1  > /sys/class/gpio/gpio33$i/value
-        sleep 0.01
-      done
-      sleep 0.5
-    for i in 7 6 5 4 3 2 1 0
-      do
-        echo  0  > /sys/class/gpio/gpio33$i/value
-        sleep 0.01
-      done
-      sleep 0.5
-  done
-}
-echo "set gpio"
-set_dir
-led_onoff
